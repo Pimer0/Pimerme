3dd372310a96d4a425ec6a299cf3ba5280c1bd24
Pimer0 <paulmerandon@gmail.com>
2024-02-27 22:25:44 +0100

    quick fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ function App() {
 
 
   return (
-    <>
+    <main className="m-0 flex items-center min-w-[320px] min-h-screen">
 {/* ici vient les components */}
 <Showcasecard/>
-    </>
+<Showcasecard/>
+<Showcasecard/>
+    </main>
   )
 }
 
