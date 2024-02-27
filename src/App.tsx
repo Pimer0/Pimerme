77b23aa16e4584c580e07693d4c1b1a285fa05da
Pimer0 <paulmerandon@gmail.com>
2024-02-27 22:35:42 +0100

    fix du style du main

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
 
   return (
-    <main className="m-0 flex items-center min-w-[320px] min-h-screen">
+    <main className="m-0 flex place-content-center min-w-[320px] h-screen py-10">
 {/* ici vient les components */}
 <Showcasecard/>
 <Showcasecard/>
--- src/components/Showcasecard.tsx
+++ src/components/Showcasecard.tsx
@@ -1,6 +1,6 @@
 function showcasecard () {
     return ( 
-        <div className="bg-slate-900 h-100 w-100 rounded-3xl border-slate-500 border"></div>
+        <div className="bg-slate-900 h-100 w-100 rounded-3xl border-slate-500 border m-10"></div>
      );
 }
 
