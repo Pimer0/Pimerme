3cf1a3197bbee23bc94a5a3f31a2dfc16aeb9ab7
Pimer0 <paulmerandon@gmail.com>
2024-02-27 22:37:28 +0100

    quick color fix

--- src/components/Showcasecard.tsx
+++ src/components/Showcasecard.tsx
@@ -1,6 +1,6 @@
 function showcasecard () {
     return ( 
-        <div className="bg-slate-900 h-100 w-100 rounded-3xl border-slate-500 border m-10"></div>
+        <div className="bg-zinc-900 h-100 w-100 rounded-3xl border-zinc-500 border m-10"></div>
      );
 }
 
